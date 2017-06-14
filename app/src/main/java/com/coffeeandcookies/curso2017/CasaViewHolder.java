6d07a93fc570c593015bd608692ffaa250bdf9c9
Gonzalo Benoffi <benoffi11@gmail.com>
2017-06-14 14:32:16 -0300

    Spinner

--- app/src/main/java/com/coffeeandcookies/curso2017/CasaViewHolder.java
+++ app/src/main/java/com/coffeeandcookies/curso2017/CasaViewHolder.java
@@ -1,6 +1,7 @@
 package com.coffeeandcookies.curso2017;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
@@ -17,6 +18,7 @@ public class CasaViewHolder extends RecyclerView.ViewHolder
     TextView textView_owner;
     CheckBox checkBox_ocupado;
     SeekBar seekBar_habitciones;
+    TextView textView_hab;
 
 
     public CasaViewHolder(View itemView) {
@@ -24,6 +26,7 @@ public class CasaViewHolder extends RecyclerView.ViewHolder
         //relacionar las variables de + arriba con sus contrapartidas en XML
         textView_calle_numero = (TextView)itemView.findViewById(R.id.textView_calle_numero);
         textView_owner = (TextView)itemView.findViewById(R.id.textView_owner);
+        textView_hab = (TextView)itemView.findViewById(R.id.textView_hab);
         checkBox_ocupado = (CheckBox)itemView.findViewById(R.id.checkBox_ocupado);
         seekBar_habitciones = (SeekBar)itemView.findViewById(R.id.seekBar_habitciones);
     }
@@ -31,9 +34,32 @@ public class CasaViewHolder extends RecyclerView.ViewHolder
     public void setItem(Casa miCasa)
     {
         textView_calle_numero.setText(miCasa.getCalle()+" "+miCasa.getNumero());
-        textView_owner.setText(miCasa.getOwner());
+        //controlo la longitud del nombre del dueño
+        if (miCasa.getOwner().length()>0) //si es mayor a 0 lo muestro
+                textView_owner.setText(miCasa.getOwner());
+        else  //sino oculto el componente
+                textView_owner.setVisibility(View.GONE);
+
         checkBox_ocupado.setChecked(miCasa.isOcupada());
+
         seekBar_habitciones.setProgress(miCasa.getHabitaciones());
+        textView_hab.setText("("+miCasa.getHabitaciones()+")");
+
+        //que no se pueda tocar
+        seekBar_habitciones.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        checkBox_ocupado.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+
     }
 
 }
--- app/src/main/java/com/coffeeandcookies/curso2017/lay_listado.java
+++ app/src/main/java/com/coffeeandcookies/curso2017/lay_listado.java
@@ -3,7 +3,14 @@ package com.coffeeandcookies.curso2017;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,12 +20,87 @@ import java.util.ArrayList;
 
 public class lay_listado extends AppCompatActivity
 {
+    String elementos_dinamicos[];
+    ArrayList<Casa> casas;
+    RecyclerView contenedor_listado;
+    Spinner spinner;
+    Button button_ver;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lay_listado);
+        crearCasas();
+        levantarComponentes();
+        setearAdaptadorLista();
+        setearAdaptadorSpinner();
+        setearEventoSpinner();
+        setearEventoBoton();
+    }
+
+    private void setearEventoBoton()
+    {
+        button_ver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                int pos = spinner.getSelectedItemPosition();
+                Toast.makeText(lay_listado.this, "Se ha seleccionado la opcion: "+elementos_dinamicos[pos], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    void setearEventoSpinner()
+    {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l)
+            {
+                Toast.makeText(lay_listado.this, "Se ha seleccionado la opcion: "+elementos_dinamicos[pos], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    void setearAdaptadorSpinner()
+    {
+        elementos_dinamicos = new String[2];
+
+        elementos_dinamicos[0] = "Elemento 1";
+        elementos_dinamicos[1] = "Elemento 2";
+
+        //String elementos_dinamicos_recursos[] = getResources().getStringArray(R.array.valores_array);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, elementos_dinamicos);
+
+
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adaptador);
+    }
+
+    private void setearAdaptadorLista()
+    {
+        AdaptadorCasas adaptadorCasas = new AdaptadorCasas(casas);
+        contenedor_listado.setAdapter(adaptadorCasas);
+    }
+
+    private void levantarComponentes()
+    {
+        contenedor_listado = (RecyclerView)findViewById(R.id.listado);
+        contenedor_listado.setLayoutManager(new LinearLayoutManager(lay_listado.this));
+        spinner= (Spinner)findViewById(R.id.spinner);
+        button_ver = (Button)findViewById(R.id.button_ver);
+    }
+
+    void crearCasas()
+    {
         //listado de casas
-        ArrayList<Casa> casas = new ArrayList<>();
+        casas = new ArrayList<>();
         //una casa
         Casa casa = new Casa();
         casa.setCalle("salta");
@@ -36,10 +118,5 @@ public class lay_listado extends AppCompatActivity
         //agrego las casas al listado
         casas.add(casa);
         casas.add(otraCasa);
-
-        RecyclerView contenedor_listado = (RecyclerView)findViewById(R.id.listado);
-        AdaptadorCasas adaptadorCasas = new AdaptadorCasas(casas);
-        contenedor_listado.setAdapter(adaptadorCasas);
-
     }
 }
